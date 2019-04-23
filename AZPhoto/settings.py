e9b94a3d3de3d0a23fe98097e24729718a9bb61e
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-04-24 03:41:00 +0630

    'a'

--- AZPhoto/settings.py
+++ AZPhoto/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Configure Django App for Heroku.
 
