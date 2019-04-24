b92ba617233ea7ea869af8432a310150a20ddbe4
AungZinTun <aungzintun2@gmail.com>
2019-04-24 17:51:07 +0630

    'a'

--- price/models.py
+++ price/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Session(models.Model):
+    name=models.CharField(max_length=30)
+    price_old=models.IntegerField()
+    price=models.IntegerField()
+    photo=models.IntegerField()
+
+    print_small_size=models.CharField(max_length=30)
+    print_small=models.IntegerField()
+
+    print_large_size=models.CharField(max_length=30)
+    print_large=models.IntegerField()
+
+    print_a=models.IntegerField()
+
+    shooting_time=models.IntegerField()
+    shooting_location=models.IntegerField()
+
+    remark=models.CharField(max_length=100)
+
+    def __str__()
+        return self.name
