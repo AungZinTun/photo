1f49334eb48891297b68aa9a9540ea7da144adf2
AungZinTun <aungzintun2@gmail.com>
2019-05-02 17:36:18 +0630

    a

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Album, Photo
 
-# Register your models here.
+# Register your models 
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+class AlbumAdmin(admin.ModelAdmin):
+    inlines=[PhotoInline,]
+
+class ImageAdmin(admin.ModelAdmin):
+    # explicitly reference fields to be shown, note image_tag is read-only
+    # fields = ( 'image_tag','album', 'lighting', 'situation', 'mood', 'fov' )
+    readonly_fields = ('image_tag',)
+
+admin.site.register(Photo, ImageAdmin)
+admin.site.register(Album, AlbumAdmin)
+
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
-# Create your models here.
+# Create your models he
+class Album (models.Model):
+    name=models.CharField(max_length=30)
+    description=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    photo=models.ImageField(upload_to='static/img/portfolio')
+    album=models.ForeignKey(Album, on_delete=models.CASCADE )
+    lighting=models.CharField(max_length=1, choices=(('u' , 'Under'), ('n', 'normal'), ('o' , 'over')) )
+    situation=models.CharField(max_length=1, choices=(('u' , 'Under'), ('n', 'normal'), ('o' , 'over')) )
+    mood=models.CharField(max_length=1, choices=(('u' , 'Under'), ('n', 'normal'), ('o' , 'over')) )
+    fov=models.CharField(max_length=1, choices=(('s' , 'Wide'), ('n', 'normal'), ('t' , 'tele')) )
+    def __str__(self):
+        return mark_safe('<img src="/%s" height="200" />' % (self.photo))
+    def image_tag(self):
+        return mark_safe('<img src="/%s" width="200" />' % (self.photo))
+        image_tag.short_description = 'Photo'
+        image_tag.allow_tags = True
+        
+      
+
+  
