0163ab7aa75284c8ad28beb04a60d2fbf4aca57b
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-08-11 07:30:31 +0630

    pages

---
+++ portfolio/choices.py
@@ -0,0 +1,7 @@
+cat_choices=[('P', 'Portrait'), 
+            ('G', 'Graduation'), 
+            ('W', 'Prewedding'), 
+            ('F', 'Family'), 
+            ('B', 'Baby'), 
+            ('C', 'Ceremony'), 
+            ('D', 'Product')]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,28 @@
+from .choices import cat_choices
 from django.shortcuts import render
+from .models import Photo , Album
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def index(request):
-    return render (request, 'portfolio/index.html')
+    photos= Photo.objects.all()
+
+    paginator= Paginator(photos,10 )
+    page=request.GET.get('page')
+    photo=paginator.get_page(page)
+    return render (request, 'portfolio/index.html', {'photo': photo})
+
+
+def cat(request):
+    photos= Photo.objects.all()
+
+    if 'cat' in request.GET:
+        cat=request.GET['cat']
+        if cat:
+            photo_cat=photos.filter(category=cat  )
+
+
+    paginator= Paginator(photo_cat,10 )
+    page=request.GET.get('page')
+    photo=paginator.get_page(page)
+    return render (request, 'portfolio/cat.html', {'photo': photo})
